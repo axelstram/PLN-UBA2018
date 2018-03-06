76b04ec4678e64e354f1befc21c44612be717b37
astraminsky <astraminsky@dc.uba.ar>
2018-03-06 10:28:21 -0300

    added stopwords filtering

--- sentiment/classifier.py
+++ sentiment/classifier.py
@@ -3,6 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
+from nltk.corpus import stopwords
 
 
 classifiers = {
@@ -23,13 +24,19 @@ def createBinaryCountPipeline(clf):
             ('clf', classifiers[clf]()),
         ])
 
+def createStopwordsPipeline(clf):
+        return Pipeline([
+            ('vect', CountVectorizer(stop_words=stopwords.words('spanish'))),
+            ('clf', classifiers[clf]()),
+        ])
+
 
 class SentimentClassifier(object):
 
     def __init__(self, clf='svm', pipeline='default'):
         """
         clf -- classifying model, one of 'svm', 'maxent', 'mnb' (default: 'svm').
-        pipeline -- type of pipeline to use: 'default', 'binary'
+        pipeline -- type of pipeline to use: 'default', 'binary', 'stopwords'
 
         """
         self._clf = clf
@@ -39,7 +46,8 @@ class SentimentClassifier(object):
             self._pipeline = createDefaultPipeline(clf)
         elif pipeline == 'binary':
             self._pipeline = createBinaryCountPipeline(clf)
-        
+        elif pipeline == 'stopwords':
+            self._pipeline = createStopwordsPipeline(clf)    
 
 
     def fit(self, X, y):
